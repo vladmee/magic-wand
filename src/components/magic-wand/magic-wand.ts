56178d72ee18680cbaecfb963a5373a5a466c458
Vlad Micliuc <vladpresedinte@gmail.com>
2023-07-07 17:20:04 +0800

    Cleanup for more abstraction

--- src/components/magic-wand/magic-wand.ts
+++ src/components/magic-wand/magic-wand.ts
@@ -1,6 +1,3 @@
 window.onload = () => {
-  let title = document.getElementById('title') as HTMLElement;
-  if (title) {
-    title.style.color = 'red';
-  }
+  // js here
 };
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,8 @@
 import magicWandHtml from 'bundle-text:./components/magic-wand/magic-wand.html';
 
-document.addEventListener('DOMContentLoaded', function () {
+window.onload = () => {
   let magicWandPlaceholder = document.getElementById('magic-wand-component');
   if (magicWandPlaceholder) {
     magicWandPlaceholder.innerHTML = magicWandHtml;
   }
-});
+};
